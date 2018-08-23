2203e409c749b27856f01df803888d0568dfa6e1
abhinav_bhardwaj <ab14bhardwaj@gmail.com>
2018-08-23 16:32:51 +0530

    KM-1352 Fix scroll issue when user send new message.

--- public/plugin/js/app/fullview/applozic.fullview.js
+++ public/plugin/js/app/fullview/applozic.fullview.js
@@ -4400,6 +4400,8 @@ var MCK_CLIENT_GROUP_MAP = [];
                         scrollTop: $mck_msg_inner.prop("scrollHeight")
                     }, 'fast');
                 }
+                var messageBody = document.querySelectorAll(".mck-message-inner.active-chat")[0];
+                messageBody.scrollTop = messageBody.scrollHeight;
                 if ($mck_tab_message_option.hasClass('n-vis')) {
                     if (msg.groupId) {
                         var group = mckGroupUtils.getGroup(msg.groupId);
